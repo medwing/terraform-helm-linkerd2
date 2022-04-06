2658f6b6e531e5a25fe9d8b47b50e457fec19b08
Sebastian Gabbert <git@huehnerhose.de>
2022-04-06 13:47:09 +0200

    handle deprecation of explicit algorithm definition

--- anchor-cert.tf
+++ anchor-cert.tf
@@ -8,7 +8,6 @@ resource "tls_private_key" "trustanchor_key" {
 
 resource "tls_self_signed_cert" "trustanchor_cert" {
   count                 = var.external_trustanchor ? 0 : 1
-  key_algorithm         = tls_private_key.trustanchor_key[0].algorithm
   private_key_pem       = tls_private_key.trustanchor_key[0].private_key_pem
   validity_period_hours = 87600
   is_ca_certificate     = true
@@ -31,7 +30,6 @@ resource "tls_private_key" "issuer_key" {
 }
 
 resource "tls_cert_request" "issuer_req" {
-  key_algorithm   = tls_private_key.issuer_key.algorithm
   private_key_pem = tls_private_key.issuer_key.private_key_pem
 
   subject {
@@ -41,7 +39,6 @@ resource "tls_cert_request" "issuer_req" {
 
 resource "tls_locally_signed_cert" "issuer_cert" {
   cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
-  ca_key_algorithm      = var.external_trustanchor ? var.trustanchor_key.algorithm : tls_private_key.trustanchor_key[0].algorithm
   ca_private_key_pem    = var.external_trustanchor ? var.trustanchor_key.private_key_pem : tls_private_key.trustanchor_key[0].private_key_pem
   ca_cert_pem           = var.external_trustanchor ? var.trustanchor_cert.cert_pem : tls_self_signed_cert.trustanchor_cert[0].cert_pem
   validity_period_hours = 8760
